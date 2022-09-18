2297e616975360e54581c98b3727c0e5cff3adbb
LAinox001 <l.antoni@hotmail.fr>
2022-09-18 11:28:20 +0200

    working canvas with many images

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,5 +4,6 @@ export * as currentword from "./currentword";
 export * as mot from "./mot";
 export * as carnet from "./carnet";
 export * as image from "./image";
+export * as images from "./images";
 export * as bonpoint from "./bonpoint";
 export * as trade from "./trade";
